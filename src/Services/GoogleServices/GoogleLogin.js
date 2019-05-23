2789fad60de432f0f92604bfb626876a8216822c
Hamdy <eng.ahmed.hr@gmail.com>
2019-05-23 12:38:35 +0200

    ios client id

--- src/Services/GoogleServices/GoogleLogin.js
+++ src/Services/GoogleServices/GoogleLogin.js
@@ -5,10 +5,10 @@ import {saveUser} from '../AuthServices'
 import { GoogleAuth } from '../FirebaseServices/GoogleAuth'
 
 
-const iOSClientId = "com.googleusercontent.apps.357729817077-pc4lpp2oramc13sopec41m5fbmdgk5ib"
+const iOSClientId = "357729817077-pc4lpp2oramc13sopec41m5fbmdgk5ib.apps.googleusercontent.com"
 const androidClientId = "357729817077-m8729cqlpeuq7ovf0skaqvmehgaa4t5n.apps.googleusercontent.com"
 const androidStandAlone = "357729817077-9cdl1mk3mkr3r204o3d216pj9onor1m7.apps.googleusercontent.com"
-const iOSStandAlone = "com.googleusercontent.apps.357729817077-j8codho5u6os0c5qf12iq968g3jh4tl9"
+const iOSStandAlone = "357729817077-j8codho5u6os0c5qf12iq968g3jh4tl9.apps.googleusercontent.com"
 
 export const LoginWithGoogle = async () => {
   try {
@@ -17,10 +17,10 @@ export const LoginWithGoogle = async () => {
     const { type, accessToken } = await Google.logInAsync(
       {
         // behavior: 'web',
+        androidStandaloneAppClientId: androidStandAlone,
         androidClientId: androidClientId,
         iosStandaloneAppClientId: iOSStandAlone,
         iosClientId: iOSClientId,
-        androidStandaloneAppClientId: androidStandAlone,
         scopes: ['profile', 'email']
       }
     )
