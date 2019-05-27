6bc9d7459e9bf7d189618c357587e491e212de0b
Hamdy <eng.ahmed.hr@gmail.com>
2019-05-27 06:51:48 +0200

    base 64 token

--- src/Services/Guid.js
+++ src/Services/Guid.js
@@ -1,6 +1,12 @@
+import { encode } from 'base-64';
 export var guidGenerator = () => {
   var S4 = function () {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
   return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+
+export var base64Token = (uid, token) => {
+  return encode(`${uid}:${token}`)
 }
