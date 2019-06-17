8956cd20a2e471a36bda2db3136c7dff929cf6a1
Hamdy <eng.ahmed.hr@gmail.com>
2019-06-17 19:15:24 +0200

    verification step 4

--- src/Screens/LawyerVerification/Step4/Step4.js
+++ src/Screens/LawyerVerification/Step4/Step4.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { navigate, goBack } from '../../../Services/NavigationServices'
+import VerifyTitle from '../../../Components/LawyerVerifyComp/VerifyStepTitle'
+import ImageIcon from '../../../Components/Common/ImageIcon'
+import { clapping } from '../../../../assets'
+import ReturnBtn from '../../../Components/LawyerVerifyComp/ReturnBtn'
 
 export default class Step4 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-        Step4
+        <VerifyTitle
+          title={'All is set up'}
+          subTitle={'We will review your submited papers to verify your account'}
+        //step={'Step 3: Firm papers'}
+        />
+        <ImageIcon
+          style={styles.clapping}
+          source={clapping}
+        />
+
+        <Text style={styles.footerText}>
+          {'Once we complete the reviewing procces we will send you an email to strat your journey'}
         </Text>
+
+        <ReturnBtn 
+          btnTitle={'Return to home'}
+          //onPress={()=>navigate('Home')}
+        />
       </View>
     );
   }
@@ -16,8 +36,23 @@ export default class Step4 extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f6f6f6',
+    backgroundColor: "#0b7f7c",
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 20
   },
+  clapping: {
+    width: 198,
+    height: 198
+  },
+  footerText: {
+    fontFamily: "Lato-Regular",
+    fontSize: 14,
+    fontWeight: "500",
+    fontStyle: "normal",
+    lineHeight: 19,
+    letterSpacing: 0,
+    textAlign: "center",
+    color: "#b9f9f7"
+  }
 });
