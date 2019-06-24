1b2fa48daad22b5c16e79c7cb9368847cf4c1d1b
Hamdy <eng.ahmed.hr@gmail.com>
2019-06-24 21:11:57 +0200

    handle login errors

--- src/Services/Login Services/MailLogin.js
+++ src/Services/Login Services/MailLogin.js
@@ -8,54 +8,62 @@ import { updateUserName } from '../FirebaseServices/UpdateUser'
 import Store from '../../Redux/Store'
 
 export const SignUp = async () => {
-  const email = Store.getState().userMail
-  const password = Store.getState().userPassword
-  const userName = Store.getState().userName
-  navigate('Spinner')
-  const {
-    currentUser,
-    userToken,
-    uid,
-    refreshToken
-  } = await MailSignUp(email, password)
+  try {
+    const email = Store.getState().userMail
+    const password = Store.getState().userPassword
+    const userName = Store.getState().userName
+    navigate('Spinner')
+    const {
+      currentUser,
+      userToken,
+      uid,
+      refreshToken
+    } = await MailSignUp(email, password)
 
-  // add userName
-  currentUser.displayName = userName
-  updateUserName(userName)
-  // let backendToken = base64Token(uid, userToken)
+    // add userName
+    currentUser.displayName = userName
+    updateUserName(userName)
+    // let backendToken = base64Token(uid, userToken)
 
-  // let userType = getUserType()
+    // let userType = getUserType()
 
-  // let pickedUser = await Register(userType, backendToken)
+    // let pickedUser = await Register(userType, backendToken)
 
-  // currentUser = Object.assign(currentUser, pickedUser)
+    // currentUser = Object.assign(currentUser, pickedUser)
 
-  await saveUser(currentUser, refreshToken)
+    await saveUser(currentUser, refreshToken)
 
-  Alert.alert('LogIn', 'You logged in successfully')
-  navigate('App')
+    Alert.alert('LogIn', 'You logged in successfully')
+    navigate('App')
+  } catch (error) {
+    alert(`${error} \nTry again`)
+  }
 }
 
 export const SignIn = async () => {
-  const email = Store.getState().userMail
-  const password = Store.getState().userPassword
+  try {
+    const email = Store.getState().userMail
+    const password = Store.getState().userPassword
 
-  navigate('Spinner')
-  const {
-    currentUser,
-    userToken,
-    uid,
-    refreshToken
-  } = await MailLogin(email, password)
+    navigate('Spinner')
+    const {
+      currentUser,
+      userToken,
+      uid,
+      refreshToken
+    } = await MailLogin(email, password)
 
-  // let backendToken = base64Token(uid, userToken)
+    // let backendToken = base64Token(uid, userToken)
 
-  // let pickedUser = await Login(backendToken)
+    // let pickedUser = await Login(backendToken)
 
-  // currentUser = Object.assign(currentUser, pickedUser)
+    // currentUser = Object.assign(currentUser, pickedUser)
 
-  await saveUser(currentUser, refreshToken)
+    await saveUser(currentUser, refreshToken)
 
-  Alert.alert('LogIn', 'You logged in successfully')
-  navigate('App')
+    Alert.alert('LogIn', 'You logged in successfully')
+    navigate('App')
+  } catch (error) {
+    alert(`${error} \nTry again`)
+  }
 }
--- src/Services/Login Services/PhoneLogin.js
+++ src/Services/Login Services/PhoneLogin.js
@@ -11,70 +11,82 @@ import Store from '../../Redux/Store'
 const captchaUrl = `https://my-lawyer-dea44.web.app/?appurl=${Linking.makeUrl('')}`
 
 export const SignUp = async (confCode, userName) => {
-  navigate('Spinner')
-  const {
-    currentUser,
-    userToken,
-    uid,
-    refreshToken
-  } = await PhoneAuth(confCode)
+  try {
+    navigate('Spinner')
+    const {
+      currentUser,
+      userToken,
+      uid,
+      refreshToken
+    } = await PhoneAuth(confCode)
 
-  // console.log('1')
-  currentUser.displayName = userName
-  updateUserName(userName)
-  // console.log('2')
-  // let backendToken = base64Token(uid, userToken)
+    // console.log('1')
+    currentUser.displayName = userName
+    updateUserName(userName)
+    // console.log('2')
+    // let backendToken = base64Token(uid, userToken)
 
-  // let userType = getUserType()
+    // let userType = getUserType()
 
-  // let pickedUser = await Register(userType, backendToken)
-  // // console.log('3')
-  // currentUser = Object.assign({},currentUser, pickedUser)
+    // let pickedUser = await Register(userType, backendToken)
+    // // console.log('3')
+    // currentUser = Object.assign({},currentUser, pickedUser)
 
-  await saveUser(currentUser, refreshToken)
-  // console.log('4')
-  Alert.alert('LogIn', 'You logged in successfully')
-  navigate('App')
+    await saveUser(currentUser, refreshToken)
+    // console.log('4')
+    Alert.alert('LogIn', 'You logged in successfully')
+    navigate('App')
+  } catch (error) {
+    alert(`${error} \nTry again`)
+  }
 }
 
 export const SignIn = async (confCode) => {
-  navigate('Spinner')
-  const {
-    currentUser,
-    userToken,
-    uid,
-    refreshToken
-  } = await PhoneAuth(confCode)
+  try {
+    navigate('Spinner')
+    const {
+      currentUser,
+      userToken,
+      uid,
+      refreshToken
+    } = await PhoneAuth(confCode)
 
-  // let backendToken = base64Token(uid, userToken)
+    // let backendToken = base64Token(uid, userToken)
 
-  // let pickedUser = await Login(backendToken)
+    // let pickedUser = await Login(backendToken)
 
-  // currentUser = Object.assign(currentUser, pickedUser)
+    // currentUser = Object.assign(currentUser, pickedUser)
 
-  await saveUser(currentUser, refreshToken)
+    await saveUser(currentUser, refreshToken)
 
-  Alert.alert('LogIn', 'You logged in successfully')
-  navigate('App')
+    Alert.alert('LogIn', 'You logged in successfully')
+    navigate('App')
+  } catch (error) {
+    alert(`${error} \nTry again`)
+  }
 }
 
 export const requestCode = async phoneNumber => {
-  let token = null
-  const listener = ({ url }) => {
-    WebBrowser.dismissBrowser()
-    const tokenEncoded = Linking.parse(url).queryParams['token']
-    if (tokenEncoded)
-      token = decodeURIComponent(tokenEncoded)
-  }
-  Linking.addEventListener('url', listener)
-  await WebBrowser.openBrowserAsync(captchaUrl)
-  Linking.removeEventListener('url', listener)
-  if (token) {
-    //fake firebase.auth.ApplicationVerifier
-    const captchaVerifier = {
-      type: 'recaptcha',
-      verify: () => Promise.resolve(token)
+  try {
+    let token = null
+    const listener = ({ url }) => {
+      WebBrowser.dismissBrowser()
+      const tokenEncoded = Linking.parse(url).queryParams['token']
+      if (tokenEncoded)
+        token = decodeURIComponent(tokenEncoded)
     }
-    await confirmationResult(phoneNumber, captchaVerifier)
+    Linking.addEventListener('url', listener)
+    await WebBrowser.openBrowserAsync(captchaUrl)
+    Linking.removeEventListener('url', listener)
+    if (token) {
+      //fake firebase.auth.ApplicationVerifier
+      const captchaVerifier = {
+        type: 'recaptcha',
+        verify: () => Promise.resolve(token)
+      }
+      await confirmationResult(phoneNumber, captchaVerifier)
+    }
+  } catch (error) {
+    alert(`${error} \nTry again`)
   }
 }
