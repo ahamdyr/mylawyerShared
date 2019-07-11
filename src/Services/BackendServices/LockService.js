cf1685cba1892716bb33dc16bf3136a1af4a0bae
Hamdy <eng.ahmed.hr@gmail.com>
2019-07-11 13:10:08 +0200

    re-ask and review answers api

--- src/Services/BackendServices/LockService.js
+++ src/Services/BackendServices/LockService.js
@@ -26,3 +26,30 @@ export const lockApi = async (questionID, accessToken) => {
     })
   })  
 }
+
+export const reAskApi = async (questionID, accessToken) => {  
+
+  return new Promise((resolve, reject) => {
+    axios.post(
+      `questions/${questionID}/locks/`,
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+    ).then((res) => {
+      if(res.data.data) {
+        //console.log('res.data.data  ',res.data)
+        resolve(res.data) 
+      }
+      else{
+        //console.log('res.data.error  ',res.data)
+        reject(res.data.error.message)
+      }
+    }).catch(err => {
+      //console.log('err  ',err)
+      reject(err)
+    })
+  })  
+}
