fd4c315bbefb7b92792bfd0f74dbacfa1c555a96
Hamdy <eng.ahmed.hr@gmail.com>
2019-07-24 11:22:47 +0200

    google sign in android bug fixed

--- src/Services/Login Services/GoogleLogin.js
+++ src/Services/Login Services/GoogleLogin.js
@@ -8,8 +8,8 @@ import { goBack, navigate } from '../NavigationServices'
 
 const iOSClientId = "357729817077-pc4lpp2oramc13sopec41m5fbmdgk5ib.apps.googleusercontent.com"
 const androidClientId = "357729817077-m8729cqlpeuq7ovf0skaqvmehgaa4t5n.apps.googleusercontent.com"
-//const androidStandAlone = "357729817077-9cdl1mk3mkr3r204o3d216pj9onor1m7.apps.googleusercontent.com"
-//const iOSStandAlone = "357729817077-j8codho5u6os0c5qf12iq968g3jh4tl9.apps.googleusercontent.com"
+const androidStandAlone = "357729817077-mp5l84a8itg794fls46bgltdg3pr6bai.apps.googleusercontent.com"
+const iOSStandAlone = "357729817077-j8codho5u6os0c5qf12iq968g3jh4tl9.apps.googleusercontent.com"
 
 export const LoginWithGoogle = async () => {
   try {
@@ -17,7 +17,8 @@ export const LoginWithGoogle = async () => {
       {
         // behavior: 'web',
         //androidStandaloneAppClientId: androidStandAlone,
-        androidClientId: androidClientId,
+        //androidClientId: androidClientId,
+        androidClientId: androidStandAlone,
         //iosStandaloneAppClientId: iOSStandAlone,
         iosClientId: iOSClientId,
         scopes: ['profile', 'email']
