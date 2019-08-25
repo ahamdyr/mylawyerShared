27f4b46d026925e25f9a496e1f8dcc9eec328e62
ahamdyr <ahamdyr@gmail.com>
2019-08-25 15:42:59 +0200

    sdk 34 update

--- src/Services/Login Services/GoogleLogin.js
+++ src/Services/Login Services/GoogleLogin.js
@@ -1,27 +1,30 @@
-import { Google } from 'expo'
+import * as Google from 'expo-google-app-auth'
+import * as AppAuth from 'expo-app-auth'
 import { GoogleAuth } from '../FirebaseServices/GoogleAuth'
 import { base64Token } from '../Guid'
 import { Register, Login } from '../BackendServices/AccountServices'
 import { saveUser, getUserType } from '../AuthServices'
-import { Alert } from 'react-native'
+import { Platform } from 'react-native'
 import { goBack, navigate } from '../NavigationServices'
+import { Linking } from 'expo'
 
 const iOSClientId = "357729817077-pc4lpp2oramc13sopec41m5fbmdgk5ib.apps.googleusercontent.com"
 const androidClientId = "357729817077-m8729cqlpeuq7ovf0skaqvmehgaa4t5n.apps.googleusercontent.com"
 const androidStandAlone = "357729817077-mp5l84a8itg794fls46bgltdg3pr6bai.apps.googleusercontent.com"
 const iOSStandAlone = "357729817077-j8codho5u6os0c5qf12iq968g3jh4tl9.apps.googleusercontent.com"
+var appRedirect = Platform.OS === 'ios' ? 'com.mylawyer.MyLawyer' : 'com.mylawyer.MyLawyerPro'
+var redirectUrl = __DEV__ ? null : `${appRedirect}:/oauth2redirect/google`
 
 export const LoginWithGoogle = async () => {
   try {
     const { type, accessToken } = await Google.logInAsync(
-      {
-        // behavior: 'web',
-        //androidStandaloneAppClientId: androidStandAlone,
-        //androidClientId: androidClientId,
-        androidClientId: androidStandAlone,
-        //iosStandaloneAppClientId: iOSStandAlone,
+      {        
+        androidStandaloneAppClientId: androidStandAlone,
+        androidClientId: androidClientId,
+        iosStandaloneAppClientId: iOSStandAlone,
         iosClientId: iOSClientId,
-        scopes: ['profile', 'email']
+        scopes: ['profile', 'email'],
+        redirectUrl: redirectUrl
       }
     )
 
