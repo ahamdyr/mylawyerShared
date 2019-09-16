f71721064bb3df088c7b1e5db04bc14e87c9759a
ahamdyr <eng.ahmed.hr@gmail.com>
2019-09-16 12:31:36 +0200

    require cycles warning fixed

--- scripts/stfu.js
+++ scripts/stfu.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const codeToObscure = /console.warn\([\s]*'Require cycle[^;]*;/;
+const codeToObscure = /console.warn\([\s\S].*"Require cycle: "/;
 const problemFilePath = './node_modules/metro/src/lib/polyfills/require.js';
 const problemFileContent = fs.readFileSync(problemFilePath,'utf8');
-fs.writeFileSync(problemFilePath, problemFileContent.replace(codeToObscure, ''), { encoding: 'utf8' });
+fs.writeFileSync(problemFilePath,problemFileContent.replace(codeToObscure,'const noConsoleWarn = (""'),'utf8');
