4aa40bd17dd2de7d3f29f5159f5cb9c4b5b80d64
ahamdyr <eng.ahmed.hr@gmail.com>
2019-09-30 18:21:28 +0200

    new phone input in sign up with mail

--- src/Components/AuthComponents/SignUpWithMailForm.js
+++ src/Components/AuthComponents/SignUpWithMailForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, ScrollView, View, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, ScrollView, View, TouchableOpacity, TextInput, Text } from 'react-native';
 import { MAIN_COLOR, WIDTH, STATUS_BAR_HEIGHT } from '../Constants'
 import SeperatorLine from '../Common/SeperatorLine'
 import LoginButton from '../Common/LoginButton'
@@ -14,7 +14,8 @@ import {
   setUserName,
   setPhoneNumber
 } from '../../Redux/Auth/actions'
-import { isValidEmailAddress } from '../../Utils/InputValidation'
+import { isValidEmailAddress, isValidPhoneNumber } from '../../Utils/InputValidation'
+import CountryPicker from "react-native-country-picker-modal";
 
 
 export default class SignUpWithMailForm extends React.PureComponent {
@@ -22,21 +23,33 @@ export default class SignUpWithMailForm extends React.PureComponent {
   phone = '';
   password = '';
   userName = '';
-  _onPhoneChange = (val) => {
-    this.phone = val
-    Store.dispatch(setPhoneNumber(val))
-  }
+  state = {
+    phone: '',
+    country: {
+      cca2: "EG",
+      callingCode: "20",
+      name: "Egypt"
+    }
+  };
+  _onChangeMobNumberText = val => {
+    this.setState({phone: val})
+    this.phone = "+" + this.state.country.callingCode + this.state.phone
+  };
+  // _onPhoneChange = (val) => {
+  //   this.phone = val
+  //   Store.dispatch(setPhoneNumber(val))
+  // }
   _onMailChange = (val) => {
     this.mail = val
-    Store.dispatch(setMail(val))
+    //Store.dispatch(setMail(val))
   }
   _onPassChange = (val) => {
     this.password = val
-    Store.dispatch(setPassword(val))
+    //Store.dispatch(setPassword(val))
   }
   _onNameChange = (val) => {
     this.userName = val
-    Store.dispatch(setUserName(val))
+    //Store.dispatch(setUserName(val))
   }
   _submit = () => {
     if (!isValidEmailAddress(this.mail)) {
@@ -61,10 +74,79 @@ export default class SignUpWithMailForm extends React.PureComponent {
       duration: 3000,
       type: 'danger',
     });
+    else if (!isValidPhoneNumber(this.phone)) {
+      showMessage({
+        message: "Provide valid phone number !",
+        hideOnPress: true,
+        duration: 3000,
+        type: "danger"
+      });
+    }
     else {
+      Store.dispatch(setMail(this.mail))
+      Store.dispatch(setPassword(this.password))
+      Store.dispatch(setPhoneNumber(this.state.phone, this.state.country.callingCode))
+      Store.dispatch(setUserName(this.userName))
       this.props.onPress()
     }
   }
+  _changeCountry = country => {
+    this.setState({ country });
+  };
+  _countryPickerFocus = () => {
+    this.countryPicker.openModal();
+  };
+
+  _renderCallingCode = () => {
+    return (
+      <TouchableOpacity 
+        style={{width: 80, alignItems: "center", marginLeft: 8}}
+        onPress={this._countryPickerFocus} 
+      >
+        <View style={styles.callingCodeView}>
+          <CountryPicker
+            ref={countryPicker => (this.countryPicker = countryPicker)}
+            closeable
+            //filterable
+            transparent
+            onChange={this._changeCountry}
+            cca2={this.state.country.cca2}
+            translation="eng"
+          />
+          <Text style={styles.nmberStyle}>
+            +{this.state.country.callingCode}
+          </Text>
+        </View>
+        <SeperatorLine style={{ width: 75 }} />
+      </TouchableOpacity>
+    );
+  };
+
+  _renderPhoneNumber = () => {
+    return (
+      <View style={{marginLeft: 15, width: 140, alignItems: "center"}}>        
+        <View style={styles.numberContainer}>
+          <TextInput
+            ref= "phoneRef"
+            name={"phoneNumber"}
+            type={"TextInput"}
+            underlineColorAndroid={"transparent"}
+            autoCapitalize={"none"}
+            autoCorrect={false}
+            onChangeText={this._onChangeMobNumberText}
+            placeholder={"Phone Number"}
+            keyboardType={'phone-pad'}
+            style={[styles.nmberStyle]}
+            returnKeyType="go"
+            selectionColor={'white'}
+            placeholderTextColor={'#ffffff'}
+            maxLength={20}
+          />
+        </View>
+        <SeperatorLine style={{ width: 135 }} />
+      </View>
+    );
+  };
   render() {
     return (
       <ScrollView
@@ -115,7 +197,12 @@ export default class SignUpWithMailForm extends React.PureComponent {
         </View>
 
         <SeperatorLine />
+
         <View style={styles.inputContainer}>
+          {this._renderCallingCode()}
+          {this._renderPhoneNumber()}
+        </View>
+        {/* <View style={styles.inputContainer}>
           <ImageIcon
             style={styles.phoneIconStyle}
             source={PhoneIcon}
@@ -133,7 +220,7 @@ export default class SignUpWithMailForm extends React.PureComponent {
           />
         </View>
 
-        <SeperatorLine />
+        <SeperatorLine /> */}
 
         <View style={styles.inputContainer}>
           <LockImage />
@@ -219,10 +306,35 @@ const styles = StyleSheet.create({
     marginLeft: 11.5
   },
   nmberStyle: {
-    fontFamily: 'Lato-Light',
-    fontSize: 16,
-    marginLeft: 18.5,
-    color: 'white',
+    fontFamily: "Lato-Light",
+    fontSize: 17,
+    marginLeft: 5,
+    color: "white"
+  },
+  countryPicker: {
+    alignItems: "center",
+    justifyContent: "space-between",
     width: 200,
+    flexDirection: "row"
+  },
+  callingCodeView: {
+    width: 70,
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  callingCodeText: {
+    fontSize: 20,
+    color: "black"
+    // fontFamily: 'Helvetica',
+    //fontWeight: 'bold',
+    //paddingRight: 10
+  },
+  numberContainer: {
+    width: 140,
+    height: 50,
+    alignItems: "flex-start",
+    justifyContent: "center"
   }
 })
